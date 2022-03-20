79b7b8fc2a7736db87b63b5bc6ae3f3bc78fd938
Ayush Sharma <ayushsharma190201@gmail.com>
2022-03-20 19:15:47 +0530

    sorted files and subfolders

--- Backend/backend.py
+++ Backend/backend.py
@@ -1,11 +1,41 @@
 import mysql.connector as mysql
 
-connection = mysql.connect(user='root',password='ayush123')
 
-cursor = connection.cursor()
-cursor.execute("show databases")
+class connection_api:
 
-for i in cursor:
-	print(i)
+	def __init__(self):
 
-connection.close()
+		self.connection = mysql.connect(user='root',password='ayush123')
+
+		self.cursor = self.connection.cursor()
+		self.cursor.execute("use inventory_man")
+		self.post_into_user_metadata()
+		self.get_user_metadata()
+		self.close_conn()
+
+	def get_user_metadata(self):
+
+		self.cursor.execute("select * from user_data")
+
+		l = [x for x in self.cursor]
+		print(l)
+
+	def post_into_user_metadata(self):
+
+		l = (input("Enter your name: "), input("Enter your username: "), input("Enter a password: ") ,input("Enter your email-id: "))
+		self.cursor.execute("insert into user_data values(%s,%s,%s,%s)",l)
+
+	def close_conn(self):
+
+		self.connection.close()
+
+def main():
+
+	main_conn = connection_api()
+
+
+if __name__ == "__main__":
+
+	main()
+
+
